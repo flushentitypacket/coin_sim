9c6151a9084eb5f1aa154296f36ce033965d3531
Ronald Hong <ronniejhong@gmail.com>
2018-02-18 13:54:59 -0500

    Why is it so effing difficult to set up linting?

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["google", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  rules: {
+    "no-trailing-spaces": 2,
+    "require-jsdoc": 0,
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import * as React from 'react'
+import * as React from 'react';
 
 export class Main extends React.PureComponent {
   render() {
-    return <div>Hello React world!</div>
+    return <div>Hello React world!</div>;
   }
 }
